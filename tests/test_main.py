7c567b4007b2d492e8f7e5cc4e2d979fd5ea7328
Jott4 <jvgcunha2002@gmail.com>
2024-10-02 20:10:15 -0300

    Refactor import statements in __init__.py and update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,7 @@
 import unittest
 from myproject.main import add, subtract
 
+
 class TestMathFunctions(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
@@ -9,6 +10,7 @@ class TestMathFunctions(unittest.TestCase):
     def test_subtract(self):
         self.assertEqual(subtract(5, 3), 2)
         self.assertEqual(subtract(1, 1), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
